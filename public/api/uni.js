a32da50c9fdad3eea24f1d0de4fcee5ff9884170
weixiaofei123 <weixiaofei19862@gmail.com>
2021-03-09 11:12:00 +1100

    update

--- public/api/uni.js
+++ public/api/uni.js
@@ -2,22 +2,22 @@
 
 {
 	"id":"1",
-	"imgURL":"HTTPS://WEIXIAOFEI123.GITHUB.IO/EXPLORATIONCHILD/src/static/images/4.jpg",
+	"imgURL":"HTTPS://WEIXIAOFEI123.GITHUB.IO/explorationChild/src/static/images/4.jpg",
 	"title":"safjskljflkajljl"
 },
 {
 	"id":"2",
-	"imgURL":"HTTPS://WEIXIAOFEI123.GITHUB.IO/EXPLORATIONCHILD/src/static/images/4.jpg",
+	"imgURL":"HTTPS://WEIXIAOFEI123.GITHUB.IO/explorationChild/src/static/images/4.jpg",
 	"title":"safjskljflkajljl"
 },
 {
 	"id":"3",
-	"imgURL":"HTTPS://WEIXIAOFEI123.GITHUB.IO/EXPLORATIONCHILD/src/static/images/5.jpg",
+	"imgURL":"HTTPS://WEIXIAOFEI123.GITHUB.IO/explorationChild/src/static/images/5.jpg",
 	"title":"safjskljflkajljl"
 },
 {
 	"id":"4",
-	"imgURL":"HTTPS://WEIXIAOFEI123.GITHUB.IO/EXPLORATIONCHILD/src/static/images/5.jpg",
+	"imgURL":"HTTPS://WEIXIAOFEI123.GITHUB.IO/explorationChild/src/static/images/5.jpg",
 	"title":"safjskljflkajljl"
 }
 
